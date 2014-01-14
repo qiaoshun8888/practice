c60a8e5300702e7f189374b39cae0aafdb0c34e1
Xin Zhou <xinzhou918@gmail.com>
2014-01-14 13:29:41 -0800

    btree traversal

--- datastruct/btree.py
+++ datastruct/btree.py
@@ -7,22 +7,57 @@ class Node(object):
 		self.right = right
 
 
-	def in_order(self):
+	def pre_order(self):
 		if self.left:
-			for i in self.left.in_order():
+			for i in self.left.pre_order():
 				yield i
 		yield self.data
+		if self.right:
+			for i in self.right.pre_order():
+				yield i
+	
+	def in_order(self):
+		yield self.data
+		if self.left:
+			for i in self.left.in_order():
+				yield i	
 		if self.right:
 			for i in self.right.in_order():
 				yield i
 
+	def post_order(self):
+		if self.right:
+			for i in self.right.post_order():
+				yield i
+		yield self.data
+		if self.left:
+			for i in self.left.post_order():
+				yield i
 
 if __name__ == '__main__':
 	n1 = Node(1)
 	n2 = Node(2)
 	n3 = Node(3)
+	n4 = Node(4)
+	n5 = Node(5)
+	n6 = Node(6)
+	n7 = Node(7)
 	n1.left = n2
 	n1.right = n3
+	n2.left = n4
+	n2.right = n5
+	n3.left = n6
+	n3.right = n7
+
+	for i in n1.pre_order():
+		print i,
+	print ""
 
 	for i in n1.in_order():
-		print i
+		print i,
+	print ""
+
+	for i in n1.post_order():
+		print i,
+	print ""
+
