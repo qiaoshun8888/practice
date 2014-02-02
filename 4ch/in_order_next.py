ed8a4eaa85f145f43d9a2221cd4a6937765c5c07
Xin Zhou <xinzhou918@gmail.com>
2014-02-01 22:40:28 -0500

    in order next, common ancestor

---
+++ 4ch/in_order_next.py
@@ -0,0 +1,18 @@
+"""
+implement next (in-order)
+"""
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from structs import btree
+
+if __name__ == '__main__':
+	l = btree.array2tree([1,2,3,4,5,6,7,8,9], True)
+	node = l[-2]
+	while node:
+		print node.data, 
+		node  = node.in_order_next()
+	print ""
+	for i in l[0].in_order():
+		print i, 
